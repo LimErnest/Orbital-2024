26cff2793bb9f079d7dabdc347ebfec54f016ebe
OttoTan <ottotanchuankai@gmail.com>
2024-05-10 22:58:13 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function AuthenticationPage() {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="none"
-              stroke="currentColor"
+              stroke="red"
               strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
