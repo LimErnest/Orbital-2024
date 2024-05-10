9c4dc72aaf5efaaedb7d1e7b431853606679724e
OttoTan <ottotanchuankai@gmail.com>
2024-05-10 17:05:55 +0800

    Modify Front Page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <section className='py-24'>
-      <div className='container'>
-        <h1 className='text-3xl font-bold'>Next TS Starter</h1>
-      </div>
-    </section>
-  )
+  return <h1 className='text-4xl'>Home</h1>;
 }
