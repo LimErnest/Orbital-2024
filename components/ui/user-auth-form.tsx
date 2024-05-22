73e66fef6666441f86ee5d3c76825238f695a260
Otto Tan <OttoTan>
2024-05-22 18:15:59 +0800

    Implemented signup passwords are equal

--- components/ui/user-auth-form.tsx
+++ components/ui/user-auth-form.tsx
@@ -1,53 +1,114 @@
-"use client"
+'use client'
 
-import * as React from "react"
+import * as React from 'react'
 
-import { cn } from "@/lib/utils"
-import { Icons } from "@/components/ui/icon"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { cn } from '@/lib/utils'
+import { Icons } from '@/components/ui/icon'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from '@/components/ui/alert-dialog'
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   const [isLoading, setIsLoading] = React.useState<boolean>(false)
+  const [password, setPassword] = React.useState<string>("")
+  const [confirmPassword, setConfirmPassword] = React.useState<string>("")
+  const [isDialogOpen, setIsDialogOpen] = React.useState<boolean>(false)
 
   async function onSubmit(event: React.SyntheticEvent) {
     event.preventDefault()
+    
+    if (password !== confirmPassword) {
+      setIsDialogOpen(true)
+      return
+    }
+    
     setIsLoading(true)
-
     setTimeout(() => {
       setIsLoading(false)
     }, 3000)
   }
 
   return (
-    <div className={cn("grid gap-6", className)} {...props}>
+    <div className={cn('grid gap-6', className)} {...props}>
       <form onSubmit={onSubmit}>
-        <div className="grid gap-2">
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="email">
+        <div className='grid gap-2'>
+          <div className='grid gap-1'>
+            <Label className='sr-only' htmlFor='email'>
               Email
             </Label>
             <Input
-              id="email"
-              placeholder="name@example.com"
-              type="email"
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
+              id='email'
+              placeholder='name@example.com'
+              type='email'
+              autoCapitalize='none'
+              autoComplete='email'
+              autoCorrect='off'
               disabled={isLoading}
+            />
+            <Label className='sr-only' htmlFor='password'>
+              Password
+            </Label>
+            <Input
+              id='password'
+              placeholder='Password'
+              type='password'
+              autoCapitalize='none'
+              autoComplete='off'
+              autoCorrect='off'
+              disabled={isLoading}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Label className='sr-only' htmlFor='password'>
+              Confirm Password
+            </Label>
+            <Input
+              id='password'
+              placeholder='Confirm Password'
+              type='password'
+              autoCapitalize='none'
+              autoComplete='off'
+              autoCorrect='off'
+              disabled={isLoading}
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
             />
           </div>
           <Button disabled={isLoading}>
             {isLoading && (
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+              <Icons.spinner className='mr-2 h-4 w-4 animate-spin' />
             )}
             Sign Up with Email
           </Button>
         </div>
       </form>
+
+      <AlertDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Passwords do not match.</AlertDialogTitle>
+            <AlertDialogDescription>
+              Please ensure that the passwords you entered match before continuing.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   )
 }
