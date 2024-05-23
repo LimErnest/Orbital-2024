a166b6c2bea491e4f44b35cb3465ea71bbb327c5
ErnestLim <e1122433@u.nus.edu>
2024-05-23 15:44:18 +0800

    Made landing page

--- app/page.tsx
+++ app/page.tsx
@@ -6,54 +6,119 @@ import { cn } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/button'
 import { UserAuthForm } from '@/components/ui/user-auth-form'
 import { Navbar } from '@/components/ui/navbar'
+import { Button } from '@/components/ui/button'
 
 export const metadata: Metadata = {
   title: 'Guidance',
   description: 'Authentication forms built using the components.'
 }
 
-export default function AuthenticationPage() {
+export default function LandingPage() {
   return (
     <>
-      <div className='container relative hidden h-[2000px] flex-col items-center justify-center rounded-md border border-gray-300 md:grid lg:max-w-none lg:px-0'>
-        <div
-          className='absolute inset-0 bg-cover bg-center'
-          style={{
-            backgroundImage:
-              'url(/img/background.jpg)',
-            zIndex: -1
-          }}
-        />
+      <div 
+        className='container relative hidden h-[4000px] flex-col items-center justify-center rounded-md border border-gray-300 md:grid lg:max-w-none lg:px-0'
+        style = {{
+          overflowY:'auto',
+          overflowX: 'hidden'
+        }}>
         <Navbar/>
         <div className='flex items-center justify-center lg:p-8'>
-          <div className='border- mx-auto flex h-[450px] w-full flex-col justify-center space-y-6 rounded-md border border-gray-300 bg-white p-4 sm:w-[450px]'>
-            <div className='flex flex-col space-y-2 text-center'>
-              <h1 className='text-2xl font-semibold tracking-tight'>
-                Create an account
-              </h1>
-              <p className='text-sm text-muted-foreground'>
-                Enter your email below to create your account
-              </p>
+          <div className ='segment-container'
+            style={{ 
+              textAlign: 'center' 
+            }}>
+            <div
+              style= {{
+                fontSize: '200px' 
+              }}>
+              <b>Guidance</b>
             </div>
-            <UserAuthForm />
-            <p className='px-8 text-center text-sm text-muted-foreground'>
-              By clicking sign up, you agree to our{' '}
-              <Link
-                href='/pages/tos'
-                className='underline underline-offset-4 hover:text-primary'
-              >
-                Terms of Service
-              </Link>{' '}
-              and{' '}
-              <Link
-                href='/pages/privacy'
-                className='underline underline-offset-4 hover:text-primary'
-              >
-                Privacy Policy
-              </Link>
-              .
-            </p>
+            <Button style = {{
+              backgroundColor: 'white',
+              color: 'black',
+              padding: '5px 10px',
+              textAlign:'center',
+              marginTop: '20px'
+            }} >
+              <div className='flex flex-col space-y-2 text-center'>
+                <Link
+                  href='/pages/signup'
+                  className='text-2xl font-semibold tracking-tight'
+                >
+                  Create an account
+                </Link>
+              </div>
+            </Button>
           </div>
+        </div>
+        
+
+        <div  
+          style = {{
+            backgroundImage: 'url(/img/background.jpg)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            width: '100vw',
+            height:'600px',
+            position:'relative',
+            left: '0',
+            padding: '2px 20px'
+          }}
+        >
+          <b>Games Available</b>
+        </div>
+
+        <div
+          style = {{
+            position: 'relative',
+            padding: '2px 20px',
+            textAlign: 'left',
+            left: '0',
+            fontSize: '50px',
+            flexDirection: 'column',
+            display: 'flex',
+            marginTop: '0',
+            marginBottom: '0',
+          }}
+        >
+          <b>What is <br/> 
+             Guidance?</b>
+          <p
+            style = {{
+              marginLeft:'auto',
+              fontSize:'14px'
+
+            }}>
+            Guidance is a games-learning platform for people to explore different games 
+            and have concise guides on those games that they wish to learn. 
+            Our platform will provide all the information that a person would need to learn the game. 
+            We would also gamify learning by providing daily quizzes that give experience points 
+            and a test to allow users to test their knowledge about the things they had learned and collect badges. 
+
+          </p>  
+
+        </div>
+
+        <div style={{ borderTop: '1px solid #ccc', margin: '20px 0' }}></div>
+
+        <div
+          style ={{
+            position: 'relative',
+            padding: '2px 20px',
+            textAlign: 'left',
+            left: '0',
+            fontSize: '50px',
+            flexDirection: 'column',
+            display: 'flex',
+            marginTop: '0',
+            marginBottom: '0'
+          }}
+        >
+          <b>Features</b>
+          <p>
+            ejiejifwefijeijfwejfwijfwjfwejewojwijfij
+          </p>
         </div>
       </div>
     </>
