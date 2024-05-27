2ec82b446841b72554da195246423a74d9633fa4
Otto Tan <OttoTan>
2024-05-27 21:06:00 +0800

    Updated AuthContext logic

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -1,14 +1,26 @@
 "use client"
-import React from 'react';
 
-export const AuthContext = React.createContext(null);
+import React, { useContext, createContext, useState, useEffect } from 'react';
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { auth } from '../../firebase/firebase';
+
+export const AuthContext = createContext({ user: null });
 
 export function AuthContextProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = React.useState(null);
+  const [user, setUser] = useState<User | null>(null); 
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {setUser(currentUser);});
+    return () => unsubscribe();
+  }, [user]);
 
   return (
-    <AuthContext.Provider value={user}>
+    <AuthContext.Provider value={{ user }}>
       {children}
     </AuthContext.Provider>
   );
+}
+
+export const UserAuth = () => {
+  return useContext(AuthContext);
 }
--- firebase/firebase.tsx
+++ firebase/firebase.tsx
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_APIKEY,
   authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
@@ -17,7 +12,6 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
