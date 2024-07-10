318563df4eb0575d57a9f864a04b865cb6ae4493
Otto Tan <OttoTan>
2024-07-10 18:49:25 +0800

    Starting to create methods for daily quiz and final quest

--- components/ui/checkuser.tsx
+++ components/ui/checkuser.tsx
@@ -3,13 +3,14 @@
 import * as React from 'react'
 import { useAuth } from '../../app/context/AuthContext'
 import { Button } from './button'
+import { Deck, randomHand } from './poker-components/card'
 
 export function CheckUser() {
   const { user } = useAuth()
 
   return (
-    <Button onClick={() => console.log(user)}>
-      <h1>Check User</h1>
+    <Button onClick={() => console.log(randomHand(5, 1))}>
+      <h1>Check deck</h1>
     </Button>
   )
 }
